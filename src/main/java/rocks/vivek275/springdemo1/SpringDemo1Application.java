5c6c0f1fcb20cae06bda0aec767298c803cde070
Vivek_275 <323103310140@gvpce.ac.in>
2025-06-04 13:08:09 +0530

    Initial Commit

---
+++ src/main/java/rocks/vivek275/springdemo1/SpringDemo1Application.java
@@ -0,0 +1,19 @@
+package rocks.vivek275.springdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringDemo1Application {
+    @GetMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemo1Application.class, args);
+    }
+
+}
