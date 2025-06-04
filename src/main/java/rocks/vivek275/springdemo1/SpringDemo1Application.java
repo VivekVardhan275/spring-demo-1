436a1418a0e3a3172b0389dfbbbc29bddf5620fe
Vivek_275 <323103310140@gvpce.ac.in>
2025-06-04 13:15:47 +0530

    Initial Commit

--- src/main/java/rocks/vivek275/springdemo1/SpringDemo1Application.java
+++ src/main/java/rocks/vivek275/springdemo1/SpringDemo1Application.java
@@ -12,6 +12,10 @@ public class SpringDemo1Application {
     public String home() {
         return "Hello World!";
     }
+    @GetMapping("/azure")
+    public String azure() {
+        return "Hello This App is Currently Deployed on Azure!";
+    }
     public static void main(String[] args) {
         SpringApplication.run(SpringDemo1Application.class, args);
     }
